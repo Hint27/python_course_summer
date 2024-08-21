413d29c91e47e3db7688aae66e2fb9290e3ddc41
Emoumo-coder <sandaumaru207@gmail.com>
2024-08-21 10:21:13 +0100

    Lesson 02

---
+++ Lesson 02.py
@@ -0,0 +1,34 @@
+print ("Umar")
+
+# More of variables
+x = 3
+y = 5
+
+print(x, y)
+# Variable Names:
+# A variable can have a short name (like x and y) or a more descriptive name
+# (age, carname, total_volume). Rules for Python variables:
+# 1. A variable name must start with a letter or the underscore character
+# 2. A variable name cannot start with a number
+# 3. A variable name can only contain alpha-numeric characters and underscores (A-z, 0-9, and _ )
+# 4. Variable names are case-sensitive (age, Age and AGE are three different variables)
+# 5. A variable name cannot be any of the Python keywords.
+
+
+# This is not a valid variable name
+#1name = "Umar"
+#name-1 = "Mark"
+#name 1 = "Samuel"
+
+#THis is valid
+name1 = "Isreal"
+name2 = "Ebuka"
+name3 = "Emmanuel"
+name_1 = "John"
+
+# Multi-word variable names
+school_name = "Pleasanton School" # Snake case
+schoolName = "Albesta Academy" #Camel Case
+SchoolName = "Hint Institute" #Pascal Case
+
+print (name1)
